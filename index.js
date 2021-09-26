0d7ebc68cbf4856eca96b972c983c7f81aac5cb5
brandon197 <brandonchan.bnc@gmail.com>
2021-09-25 23:45:57 -0400

    fixed contact icons and added prog bar

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ const typeWriter = () => {
     setTimeout(typeWriter, speed);
   }
 }
+
+window.onscroll = function() {progress()};
+function progress() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
 typeWriter();
 navSlide();
 
